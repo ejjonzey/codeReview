a7bd9abf3269e015af0c04100870bffb11347d45
Ejjonzey <ejjonzey1982@gmail.com>
2018-03-08 22:40:43 -0500

    three triangle test made and found.  Need to figure out impossible triangle

--- triangle/spec/TriangleSpec.js
+++ triangle/spec/TriangleSpec.js
@@ -1,5 +1,12 @@
 describe("Answer", function () {
     it("Should return Triangle is Equilateral when all sides are equal", function () {
-        expect(TriangleType(1, 1, 1)).toEqual("Triangle is Equilateral");
+        expect(TriangleType(1, 1, 1)).toBe("Triangle is Equilateral");
     });
+    it("Should return Triangle is Isosceles when two sides are equal", function (){
+        expect(TriangleType(1, 1, 2)).toBe("Triangle is Isosceles");
+    });
+    it("Should return Triangle is Scalene when no sides are equal", function () {
+        expect(TriangleType(1, 2, 3)).toBe("Triangle is Scalene")
+    });
+    it("Should return Triangle Cannot Exist")
 })
--- triangle/src/Triangle.js
+++ triangle/src/Triangle.js
@@ -1,7 +1,12 @@
 function TriangleType(sideA, sideB, sideC ){
     if (sideA === sideB && sideA === sideC) {
-        console.log("triangle is Equilateral");
-        return ("Triangle is Equilateral");
-    }
-    
+        console.log("Triangle is Equilateral");
+        return "Triangle is Equilateral";
+    } else if (sideA != sideB && sideA != sideC && sideB != sideC) {
+        console.log("Triangle is Scalene");
+        return "Triangle is Scalene";
+    }  else if (sideA === sideB && sideA != sideC) {
+        console.log("Triangle is Isosceles");
+        return "Triangle is Isosceles";
+    }  
 }
