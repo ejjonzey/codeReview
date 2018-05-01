c19f10148f45f9d435bc4b39317305ae553e58fd
Ejjonzey <ejjonzey1982@gmail.com>
2018-04-30 20:03:20 -0400

    i cant figure out how to set up a map

--- triangle/src/toRoman.js
+++ triangle/src/toRoman.js
@@ -1,33 +1,29 @@
-// const arabicToRoman = {
-//     1: "I",
-//     4: "IV",
-//     5: "V",
-//     9: "IX",
-//     10: "X",
-//     40: "XL",
-//     50: "L",
-//     90: "XC",
-//     100: "C",
-//     400: "CD",
-//     500: "D",
-//     900: "CM",
-//     1000: "M"
-//     };
-
-// var toRoman = arabicToRoman.map(function() {
-
-// });
-function toRoman(num){
-    let romanNumeral = '';
-    const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    const roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-    for ( i = 0; i <= decimal.length; i++) {
-        while (num % decimal[i] < num) {
-            romanNumeral += roman[i];
-            num -= decimal[i];
-        } 
+function toRoman(num) {
+    const arabicToRoman = {
+      1000: 'M',
+      900: 'CM',
+      500: 'D',
+      400: 'CD',
+      100: 'C',
+      90: 'XC',
+      50: 'L',
+      40: 'XL',
+      10: 'X',
+      9: 'IX',
+      5: 'V',
+      4: 'IV',
+      1: 'I'
     };
-    return romanNumeral;
-}
-
- 
+    return Object.keys(arabicToRoman)
+       .reverse()
+       .reduce((romanNumeral, decimalNumber) => {
+         let initialString = '';
+         while (num % decimalNumber < num) {
+           initialString += arabicToRoman[decimalNumber];
+           num -= decimalNumber;
+         }
+         return romanNumeral + initialString;
+       }, '');
+  }
+toRoman(1136);
+  
