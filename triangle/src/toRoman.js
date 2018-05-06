085309b0720314179d5f2002d6699ba3cbf456d2
Ejjonzey <ejjonzey1982@gmail.com>
2018-05-06 10:47:18 -0400

    refactored conditionals, renamed variables, need a nudge in the right direction for reworking tests

--- triangle/src/toRoman.js
+++ triangle/src/toRoman.js
@@ -1,29 +1,42 @@
 function toRoman(num) {
-    const arabicToRoman = {
-      1000: 'M',
-      900: 'CM',
-      500: 'D',
-      400: 'CD',
-      100: 'C',
-      90: 'XC',
-      50: 'L',
-      40: 'XL',
-      10: 'X',
-      9: 'IX',
-      5: 'V',
-      4: 'IV',
-      1: 'I'
-    };
-    return Object.keys(arabicToRoman)
-       .reverse()
-       .reduce((romanNumeral, decimalNumber) => {
-         let initialString = '';
-         while (num % decimalNumber < num) {
-           initialString += arabicToRoman[decimalNumber];
-           num -= decimalNumber;
-         }
-         return romanNumeral + initialString;
-       }, '');
-  }
-toRoman(1136);
-  
+  const arabicToRoman = {
+    1000: 'M',
+    900: 'CM',
+    500: 'D',
+    400: 'CD',
+    100: 'C',
+    90: 'XC',
+    50: 'L',
+    40: 'XL',
+    10: 'X',
+    9: 'IX',
+    5: 'V',
+    4: 'IV',
+    1: 'I'
+  };
+  return Object
+    .keys(arabicToRoman)
+    .reverse()
+    .reduce((romanNumeral, decimalNumber) => {
+      let initialRomanNumeral = '';
+      while (romanNumeralConverter(num, decimalNumber)) {
+        initialRomanNumeral += arabicToRoman[decimalNumber];
+        num = romanNumeralRemainder(num, decimalNumber);
+      }
+      return finalRomanNumeral(romanNumeral, initialRomanNumeral);
+    }, '');
+}
+
+function finalRomanNumeral(romanNumeral, initialRomanNumeral) {
+  return romanNumeral + initialRomanNumeral;
+}
+
+function romanNumeralRemainder(num, decimalNumber) {
+  num -= decimalNumber;
+  return num;
+}
+
+function romanNumeralConverter(num, decimalNumber) {
+  return num % decimalNumber < num;
+}
+
