66124e332ff4299f24bf304f3e81375f1c92ff23
Ejjonzey <ejjonzey1982@gmail.com>
2018-05-17 22:35:21 -0400

    Were you suggesting nested tests?

--- triangle/spec/toRomanSpec.js
+++ triangle/spec/toRomanSpec.js
@@ -1,64 +1,46 @@
 describe('Roman Numeral Converter', () => {
-    it("Should convert the number 1 into Roman Numeral I", function () {
-        expect(toRoman(1)).toEqual('I');
+
+    describe('Base minus 1', () => {
+        it('Should return 1 before the closest base when given 4 or 9', () => {
+            expect(toRoman(4)).toEqual("IV");
+            expect(toRoman(9)).toEqual("IX");
+        });
     });
 
-    it("Should subtract 1 from 5 to convert the number 4 into Roman numeral IV", function ()  {
-        expect(toRoman(4)).toEqual('IV');
+    describe('Base plus 1', () => {
+        it('Should return 1 after the closest base when given 6 or 11', () => {
+            expect(toRoman(6)).toEqual('VI');
+            expect(toRoman(11)).toEqual('XI');
+        });
     });
 
-    it("Should convert the number 5 into Roman Numeral V", function () {
-        expect(toRoman(5)).toEqual('V');
+    describe('Base minus 10', () => {
+        it('Should return 10 before the closest base when given 40 or 90', () => {
+            expect(toRoman(40)).toEqual('XL');
+            expect(toRoman(90)).toEqual('XC');
+        });
     });
 
-    it("Should add 1 to 5 to convert the number 6 into Roman Numeral VI", function () {
-        expect(toRoman(6)).toEqual('VI');
+    describe('Base plus 10', () => {
+        it('Should return 10 after the closest base when given 60 or 110', () => {
+            expect(toRoman(60)).toEqual('LX');
+            expect(toRoman(110)).toEqual('CX');
+        });
     });
 
-    it("Should subtract 1 from 10 to convert the number 9 into Roman Numeral IX", function () {
-        expect(toRoman(9)).toEqual('IX');
+    describe('Base minus 100', () => {
+        it('Should return 100 before the closest base when given 400 or 900', () => {
+            expect(toRoman(400)).toEqual('CD');
+            expect(toRoman(900)).toEqual('CM');
+        });
     });
 
-    it("Should convert the number 10 into Roman Numeral X", function () {
-        expect(toRoman(10)).toEqual('X');
+    describe('Base plus 100', () => {
+        it('Should return 100 after the closest base when given 600 or 1100', () => {
+            expect(toRoman(600)).toEqual('DC');
+            expect(toRoman(1100)).toEqual('MC');
+        });
     });
-
-    it("Should add 1 to 10 to convert the number 11 into Roman Numeral XI", function () {
-        expect(toRoman(11)).toEqual('XI');
-    });
-
-    it("Should convert the number 40 into Roman Numeral XL", function () {
-        expect(toRoman(40)).toEqual('XL');
-    });
-
-    it("Should convert the number 50 into Roman Numeral L", function () {
-        expect(toRoman(50)).toEqual('L');
-    });
-
-    it("Should convert the number 90 into Roman Numeral XC", function () {
-        expect(toRoman(90)).toEqual('XC');
-    });
-        
-    it("Should convert the number 100 into Roman Numeral C", function () {
-        expect(toRoman(100)).toEqual('C');
-    });
-
-    it("Should convert the number 400 into Roman Numeral CD", function () {
-        expect(toRoman(400)).toEqual('CD');
-    });
-       
-    it("Should convert the number 500 into Roman Numeral D", function () {
-         expect(toRoman(500)).toEqual('D');
-    });
-
-    it("Should convert the number 900 into Roman Numeral CM", function () {
-        expect(toRoman(900)).toEqual('CM');
-    });
-
-    it("Should convert the number 1000 into Roman Numeral M", function () {
-        expect(toRoman(1000)).toEqual('M');
-    });
-
     it("Should convert the number 1377 into Roman Numeral MCCCXXLVII", function () {
         expect(toRoman(1377)).toEqual('MCCCLXXVII');
 
