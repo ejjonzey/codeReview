365aa439263179003652406b966d47a8484a3890
Ejjonzey <ejjonzey1982@gmail.com>
2018-07-12 19:10:22 -0400

    first feedback completed, need clarification on final test.

--- triangle/spec/checkCashRegisterSpec.js
+++ triangle/spec/checkCashRegisterSpec.js
@@ -1,20 +1,152 @@
 describe('Check cash register for change', () => {
-    
+
     describe('Insufficient Funds, not enough money for change', () => {
         it('Should return "Insufficient Funds" when cash in drawer is less than change due', () => {
-          expect(checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])).toEqual("Insufficient Funds");  
+            expect(checkCashRegister(19.50, 20.00, [
+                [
+                    "PENNY", 0.01
+                ],
+                [
+                    "NICKEL", 0
+                ],
+                [
+                    "DIME", 0
+                ],
+                [
+                    "QUARTER", 0
+                ],
+                [
+                    "ONE", 0
+                ],
+                [
+                    "FIVE", 0
+                ],
+                [
+                    "TEN", 0
+                ],
+                [
+                    "TWENTY", 0
+                ],
+                ["ONE HUNDRED", 0]
+            ])).toEqual("Insufficient Funds");
         });
     });
 
     describe('Closed, change and cash in drawer are equal', () => {
         it('Should return "Closed" when cash in drawer is equal to change due', () => {
-            expect(checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])).toEqual("Closed");
+            expect(checkCashRegister(19.50, 20.00, [
+                [
+                    "PENNY", 0.50
+                ],
+                [
+                    "NICKEL", 0
+                ],
+                [
+                    "DIME", 0
+                ],
+                [
+                    "QUARTER", 0
+                ],
+                [
+                    "ONE", 0
+                ],
+                [
+                    "FIVE", 0
+                ],
+                [
+                    "TEN", 0
+                ],
+                [
+                    "TWENTY", 0
+                ],
+                ["ONE HUNDRED", 0]
+            ])).toEqual("Closed");
         });
     });
 
     describe('Open, cash in drawer is greater than change', () => {
         it('Should return array when change in drawer is greater than change due', () => {
-            expect(checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]])).toEqual([["TWENTY", 60.00], ["TEN", 20.00], ["FIVE", 15.00], ["ONE", 1.00], ["QUARTER", 0.50], ["DIME", 0.20], ["PENNY", 0.04]]);
+            expect(checkCashRegister(3.26, 100.00, [
+                [
+                    "PENNY", 1.01
+                ],
+                [
+                    "NICKEL", 2.05
+                ],
+                [
+                    "DIME", 3.10
+                ],
+                [
+                    "QUARTER", 4.25
+                ],
+                [
+                    "ONE", 90.00
+                ],
+                [
+                    "FIVE", 55.00
+                ],
+                [
+                    "TEN", 20.00
+                ],
+                [
+                    "TWENTY", 60.00
+                ],
+                ["ONE HUNDRED", 100.00]
+            ])).toEqual([
+                [
+                    "TWENTY", 60.00
+                ],
+                [
+                    "TEN", 20.00
+                ],
+                [
+                    "FIVE", 15.00
+                ],
+                [
+                    "ONE", 1.00
+                ],
+                [
+                    "QUARTER", 0.50
+                ],
+                [
+                    "DIME", 0.20
+                ],
+                [
+                    "PENNY", 0.04
+                ]
+            ]);
+        });
+    });
+
+    describe('Closed, incorrect money in drawer to make change', () => {
+        it('Should return "Closed" when cash in drawer cannot make proper change', () => {
+            expect(checkCashRegister(15, 20, [
+                [
+                    "PENNY", 0
+                ],
+                [
+                    "NICKEL", 0
+                ],
+                [
+                    "DIME", 0
+                ],
+                [
+                    "QUARTER", 0
+                ],
+                [
+                    "ONE", 0
+                ],
+                [
+                    "FIVE", 0
+                ],
+                [
+                    "TEN", 10.00
+                ],
+                [
+                    "TWENTY", 0
+                ],
+                ["ONE HUNDRED", 0]
+            ])).toEqual("Closed")
         });
     });
 });
--- triangle/src/checkCashRegister.js
+++ triangle/src/checkCashRegister.js
@@ -3,35 +3,35 @@ function checkCashRegister(price, cash, cashInDrawer) {
     const denominations = [
         {
             name: 'ONE HUNDRED',
-            value: 100.00
+            amount: 100.00
         }, {
             name: 'TWENTY',
-            value: 20.00
+            amount: 20.00
         }, {
             name: 'TEN',
-            value: 10.00
+            amount: 10.00
         }, {
             name: 'FIVE',
-            value: 5.00
+            amount: 5.00
         }, {
             name: 'ONE',
-            value: 1.00
+            amount: 1.00
         }, {
             name: 'QUARTER',
-            value: 0.25
+            amount: 0.25
         }, {
             name: 'DIME',
-            value: 0.10
+            amount: 0.10
         }, {
             name: 'NICKEL',
-            value: 0.05
+            amount: 0.05
         }, {
             name: 'PENNY',
-            value: 0.01
+            amount: 0.01
         }
     ];
 
-    let change = cash - price;
+    let amountLeft = cash - price;
 
     let register = cashInDrawer.reduce(function (accumulator, current) {
         accumulator.total += current[1];
@@ -39,73 +39,51 @@ function checkCashRegister(price, cash, cashInDrawer) {
         return accumulator;
     }, {total: 0});
 
-    if (Closed(register, change)) {
+    if (closedRegister(register, amountLeft)) {
         return 'Closed';
     }
 
-    if (insufficientFunds(register, change)) {
+    if (insufficientFunds(register, amountLeft)) {
         return 'Insufficient Funds';
     }
 
     let customerChange = denominations.reduce(function (accumulator, current) {
-        let value = 0;
-        while (changeFromRegister(register, current, change)) {
-            change -= current.value;
-            register[current.name] -= current.value;
-            value += current.value;
+        let amount = 0;
+        while (changeFromRegister(register, current, amountLeft)) {
+            amountLeft -= current.amount;
+            register[current.name] -= current.amount;
+            amount += current.amount;
 
-            change = Math.round(change * 100) / 100;
+            roundAmountLeft();
         }
 
-        if (value > 0) {
-            accumulator.push([current.name, value]);
+        if (amount > 0) {
+            accumulator.push([current.name, amount]);
         }
         return accumulator;
     }, []);
 
-    if (customerChange.length < 1 || change > 0) {
+    if (customerChange.length < 1 || amountLeft > 0) {
         return "Insufficient funds"
     }
 
     return customerChange;
+
+    function roundAmountLeft() {
+        amountLeft = Math.round(amountLeft * 100) / 100;
+    }
 }
 
-checkCashRegister(3.26, 100.00, [
-    [
-        "PENNY", 1.01
-    ],
-    [
-        "NICKEL", 2.05
-    ],
-    [
-        "DIME", 3.10
-    ],
-    [
-        "QUARTER", 4.25
-    ],
-    [
-        "ONE", 90.00
-    ],
-    [
-        "FIVE", 55.00
-    ],
-    [
-        "TEN", 20.00
-    ],
-    [
-        "TWENTY", 60.00
-    ],
-    ["ONE HUNDRED", 100.00]
-]);
 
-function changeFromRegister(register, current, change) {
-    return register[current.name] > 0 && change >= current.value;
+function changeFromRegister(register, current, amountLeft) {
+    return register[current.name] > 0 && amountLeft >= current.amount;
 }
 
-function insufficientFunds(register, change) {
-    return register.total < change;
+function insufficientFunds(register, amountLeft) {
+    return register.total < amountLeft;
 }
 
-function Closed(register, change) {
-    return register.total === change;
+function closedRegister(register, amountLeft) {
+    return register.total === amountLeft;
 }
+
